7dadeb4a9096a435e3c3fdd7ecefe449bd3fbe71
Md. Roman Islam Ali <107925118+romanislamali@users.noreply.github.com>
2023-03-03 12:50:48 +0600

    final commit

--- BookingInnUI/src/app/management/location/location-update/location-update.component.ts
+++ BookingInnUI/src/app/management/location/location-update/location-update.component.ts
@@ -9,32 +9,52 @@ import { LocationService } from 'src/app/service/location.service';
   styleUrls: ['./location-update.component.css']
 })
 export class LocationUpdateComponent {
-l : Location = new Location();
+  l: Location = new Location();
 
-id: number = 0;
-constructor(private locationService: LocationService,
-  private route: ActivatedRoute,
-  private router: Router
-) { }
+  id: number = 0;
+  constructor(private locationService: LocationService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) { }
 
-ngOnInit(): void {
-  this.id = this.route.snapshot.params['id'];
-  this.locationService.getLocationById(this.id).subscribe(
-    data => {
-      this.l = data;
-    }
-  );
-}
+  ngOnInit(): void {
+    this.id = this.route.snapshot.params['id'];
+    this.locationService.getLocationById(this.id).subscribe(
+      data => {
+        this.l = data;
+      }
+    );
+  }
 
 
-
-  locationUpdate(){
+  locationUpdate() {
     this.locationService.updateLocation(this.id, this.l).subscribe(
       data => alert('Location updated successfully!!'),
-       error => alert('Something is wrong, please try again!!'));
-      //  this.ngOnInit();
+      error => alert('Something is wrong, please try again!!'));
+    //  this.ngOnInit();
     this.router.navigate(['/alllocation']);
 
   }
+
+  getLid: number;
+  deleteL() {
+    let confirmAction = confirm("Are you sure to delete this location?");
+    if (confirmAction) {
+      this.getLid = this.id
+      this.deleteLocation(this.getLid);
+    }
+  }
+
+  deleteLocation(id: number) {
+    console.log(id);
+    this.locationService.deleteLocationById(id).subscribe(
+      data => {
+        alert('Location successfully deleted!!');
+      },
+      error => alert('Somethig wrong, please try again!!')
+    );
+  }
+
+
 
 }
